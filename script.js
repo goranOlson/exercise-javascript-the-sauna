9ab809e2d6799bda49fdb6b37949d45dc3052f5e
Göran Olson <goran@famolson.se>
2024-01-17 11:31:19 +0100

    changed variable and function names

--- script.js
+++ script.js
@@ -1,36 +1,42 @@
-
-
 let input;
 let message = 'Select temperature (Fahrenheit)';
+let fahrenheit;
 let celsius;
 
 do {
+    // Ask for temperature (fahrenheit)
     input = prompt(message);
-
-    celsius = fahrenheitToCelsius( parseInt(input) );
-
-    message = getRespons(celsius);
+    fahrenheit = parseInt(input);
+    // Get temperature in Celsius
+    celsius = fahrenheitToCelsius(fahrenheit);
+    // Produce user message if needed
+    message = createMessage(fahrenheit, celsius);
 } while (message);
 
-alert('The temperature is good!');
+// Responce to the user
+message = `Temperature ${input} degrees Fahrenheit`;
+alert(message + ' is good!');
 
+// Log data
+message += ` (~ ${celsius.toFixed(1)} celsius) is accepted`;
+console.log(message);
 
 
 function fahrenheitToCelsius(f) {
     celsius = (f - 32) / 1.8;
-     console.log(`fahrenheitToCelsius(${f}) => ${celsius}`);
+    // console.log(`fahrenheitToCelsius(${f}) => ${celsius}`);
     return celsius;
 }
 
-function getRespons(celsius) {
+function createMessage(fahrenheit, celsius) {
     let response;
 
     if (celsius < 73) {
-        response = "To low temperature, please increase";
+        response = `${fahrenheit} degrees Fahrenheit is to low temperature, please increase`;
     } else if (celsius > 77) {
-        response = "To high temperature, please decrease";
+        response = `${fahrenheit} degrees Fahrenheit is to high temperature, please decrease`;
     }
-    console.log(`getRespons(${celsius}) => '${response}'`);
+    // console.log(`createMessage(${celsius}) => '${response}'`);
 
     return response;
 }
